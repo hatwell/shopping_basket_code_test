d628e1e37e867170e628632ffffbd2e5614f034d
hatwell <caroline.hatwell@gmail.com>
2017-05-07 15:18:08 +0100

    commented code

--- shopping_basket/Basket.java
+++ shopping_basket/Basket.java
@@ -8,19 +8,25 @@ public class Basket {
     this.items = new ArrayList<Item>();
   }
 
+  //returns array of items
   public ArrayList<Item> getItems(){
     return this.items;
   }
 
+  //returns number of items in basket
   public int getItemCount(){
     return items.size();
   }
 
+  //adds an item to the basket
   public void addToBasket(Item item){
     items.add(item);
   }
 
+  //remove all items from the basket
   public void emptyBasket(){
     items.clear();
   }
+
+
 }
